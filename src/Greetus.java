69c6c9e562839f48c3ea90719684aa026d7c42b3
Magdalena Dzindzisz <dzlica@gmail.com>
2017-11-28 12:03:53 +0100

    a5

--- src/Greetus.java
+++ src/Greetus.java
@@ -1,9 +1,9 @@
 public class Greetus {
 
-    public String Greet(String greet) {
+    public String Greet(String greet, String greeta) {
 
-        greet = "Hello Barbi!" + " Hii Lica!";
-        return greet;
-
+        greet = "Hello Barbi!";
+        greeta = "Hii Lica!";
+        return greet + greeta;
     }
 }
