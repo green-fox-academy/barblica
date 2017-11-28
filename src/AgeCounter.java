e4ff31c2902c28d0e5a51c92449245e56a2a030c
Magdalena Dzindzisz <dzlica@gmail.com>
2017-11-28 11:38:57 +0100

    a4

--- src/AgeCounter.java
+++ src/AgeCounter.java
@@ -1,13 +1,15 @@
 
 public class AgeCounter {
 
-    public String count(int age) {
-        return String.valueOf(age);
+    public String count(int age, int month) {
+        return String.valueOf(age) + String.valueOf(month);
     }
 
     public static void main(String[] args) {
+
         AgeCounter counter = new AgeCounter();
         int age = 27;
-        System.out.println(counter.count(age));
+        int month = 8;
+        System.out.println(counter.count(age, month));
     }
 }
--- src/Greetus.java
+++ src/Greetus.java
@@ -5,5 +5,5 @@ public class Greetus {
         return greet;
     }
 
-    
+
 }
